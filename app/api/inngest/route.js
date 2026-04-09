555297485cf4ed5bf413f5b67b123ecca23780cf
kapilk34 <kapilkumargupta1511@gmail.com>
2026-04-09 21:00:04 +0530

    hello

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,8 @@
-import { server } from "inngest/next";
+import { serve } from "inngest/next";  // ✅ correct
 import { inngest } from "@/lib/inngest/client";
 
-//create an API that serves zero functions
+// create API
 export const { GET, POST, PUT } = serve({
-    client: inngest,
-    functions: [
-        /*your function will be passed here later! */
-    ],
+  client: inngest,
+  functions: [],
 });
